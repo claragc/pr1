83fb2f596662a1663370dcfc821e02ed8b1a0aad
claragc <clara.garcia@babel.es>
2018-02-20 19:55:18 +0100

    modulo iniciales

---
+++ lib/m1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+def saludar(saludo):
+	print saludo
+
+def iniciales(nombre,ape1,ape2): 
+	iniciales=nombre[0]+'.'+ape1[0]+'.'+ape2[0]+'.'
+	print "Tus inciales son: "+iniciales.upper()
