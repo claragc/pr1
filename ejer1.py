0b01693606fed3ae5a6b4ec57ae8f962eea599c3
claragc <clara.garcia@babel.es>
2018-02-20 16:03:30 +0100

    Mis dos primeros programas en python

---
+++ ejer1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+x=int(raw_input('Introduce un número:'))
+
+if x>10:
+  print 'Mayor'
+else:
+  print 'Menor'
+
---
+++ ejer2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+x=int(raw_input('Dime un número y te diré si es par o impar: '))
+
+if x%2==0:
+  print 'Es par'
+else:
+  print 'Es impar'
+
