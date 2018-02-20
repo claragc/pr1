5443abb5d6afefcf418e02c49ac1cf202eb4812a
claragc <clara.garcia@babel.es>
2018-02-20 19:07:32 +0100

    pasar a mayusculas

---
+++ ejer9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Pedir nombre y ponerlo en mayúsculas
+
+#import os
+import sys
+
+print sys.argv[0]
+print sys.argv[1]
+print "Tu nombre en mayúsculas es: "+sys.argv[1].upper()
+
